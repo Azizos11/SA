49e35f99c4dd3dac20ed72a0de2a1f50e5fba513
Azizos11 <azizeelkateb6@gmail.com>
2022-01-31 14:10:51 +0100

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import styled from 'styled-components'
+import { Badge } from '@material-ui/core';
+import { Search, ShoppingCartOutlined } from '@material-ui/icons';
+import React from 'react';
+import styled from 'styled-components';
 
 const Container = styled.div`
     height : 60px;
@@ -9,6 +11,7 @@ const Container = styled.div`
     const Wrapper = styled.div`
     padding: 10px 20px;
     display: flex;
+    align-items: center;
     justify-content: space-between;
 
     `;
@@ -16,19 +19,54 @@ const Container = styled.div`
 
     const Left = styled.div `
     flex: 1;
+    display: flex;
+    align-items: center;
     `;
 
     const Language = styled.span `
     font-size: 14px;
     cursor : pointer;
+    `;
+
+    const SearchContainer = styled.div `
+    border: 0.5px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left:25px
+    padding: 5px;
+    
     `
+
+    const Input = styled.input`
+    border: none;
+    
+    `;
+
 
     const Center = styled.div `
     flex: 1;
+    text-align: center;
     `;
+
+    const Logo = styled.h1`
+    font-weight: bold;
+    `;
+
+
     const Right = styled.div `
     flex: 1;
+    display: flex;
+    text-align: center;
+    justify-content:flex-end;
+    
     `;
+
+    const MenuItem = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+    margin-left:25px;
+
+    `
     
 
    const Navbar = () => {
@@ -37,12 +75,22 @@ const Container = styled.div`
           <Left>
               <Language>EN</Language>
               <SearchContainer>
-                  input
-                  icon
+                  <Input/>
+                  <Search style={{color:"gray", fontSize:16}}/>
               </SearchContainer>
           </Left>
-          <Center>Center</Center>
-          <Right>Right</Right>
+          <Center><Logo>SA.</Logo></Center>
+          <Right>
+
+            <MenuItem>REGISTER</MenuItem>
+            <MenuItem>SIGN IN</MenuItem> 
+            <MenuItem>
+            <Badge badgeContent={4} color="primary">
+      <ShoppingCartOutlined color="action" />
+    </Badge>
+            </MenuItem> 
+
+          </Right>
       </Wrapper>
       </Container>;
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import Announcement from '../components/Announcement';
 import Navbar from '../components/Navbar';
 const Home = () => {
   return <div className="container">
+    <Announcement/>
      <Navbar/>
   </div>;
 };
